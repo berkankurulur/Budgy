fa48416d6396a4dd1daa18d105ef339a039a4b05
Berkan Kurulur <kurulurberkan@gmail.com>
2026-05-20 12:01:59 +0200

    Initial Commit

---
+++ Budgy/BudgyApp.swift
@@ -0,0 +1,17 @@
+//
+//  BudgyApp.swift
+//  Budgy
+//
+//  Created by Berkan Kurulur on 20.05.2026.
+//
+
+import SwiftUI
+
+@main
+struct BudgyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
